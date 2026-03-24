3b39c49daa0772bdd2758016ae322e133b0bb1da
Utsavthebug <neupaneutsav.2001@gmail.com>
2026-03-24 16:20:32 +0545

    home page added

---
+++ resources/views/front/partials/masterclass/home.blade.php
@@ -0,0 +1,52 @@
+<main class="hero-container">
+    <section class="content-side">
+            <div class="text-wrapper">
+                <p class="overline">LFG PUBLISH</p>
+                <h1 class="serif">From Idea to   <br/><span class="highlight">Published</span> book</h1>
+                
+            </div>
+            
+            <div>
+                <p class="tagline">"Free Masterclass for Those Who Are Ready to Become Bestselling Authors"</p>
+
+                <ul class="masterclass-description">
+                <li>
+                     Step into the identity of a bestselling author - Activation meditation you take home!
+                </li>   
+
+ <li>
+ Experience the Write Naked method
+</li>
+
+
+ <li>
+Write your first raw, powerful page
+ </li>
+
+
+ <li>
+Understand how to go from idea to publishing on Amazon
+ </li>
+
+ <li>
+Learn what actually creates a bestselling book
+
+ </li>
+</ul>
+            
+            <a href="#" class="cta-button">
+                    Sign up <span>&rarr;</span>
+                </a>
+
+            </div>
+
+        </section>
+
+        <section class="image-side">
+            <img 
+            src="{{ asset('images/LFG.png') }}"
+             alt="People talking">
+        </section>
+        
+    
+    </main>
